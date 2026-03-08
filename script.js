1029a28c201c8b7a546241e8bcdd073d40ee7201
nazrul23-human <nazrul23-human@gmail.com>
2026-03-08 12:32:35 +0600

    Add login function

---
+++ script.js
@@ -0,0 +1,16 @@
+function login(){
+
+let username = document.getElementById("username").value
+let password = document.getElementById("password").value
+
+if(username === "admin" && password === "admin123"){
+
+window.location.href="dashboard.html"
+
+}else{
+
+alert("Invalid username or password")
+
+}
+
+}
